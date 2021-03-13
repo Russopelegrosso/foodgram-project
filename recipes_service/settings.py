0b163ac4fcd2c9ff5bc1f7f7a420a370add78ad5
Arsen Diubua <arsenium1941@gmail.com>
2021-03-13 23:41:59 +0500

    fix_flow

--- recipes_service/settings.py
+++ recipes_service/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = '(e4((3w)ri^6*+mmm%$q@z2u7xp%vs1jklag-*%kc6d7i2-dz('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '178.154.212.92']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','178.154.212.92']
 
 
 # Application definition
