3b86ac10e78e17d8890c84f4d3fe748bf9d308b6
Arsen Diubua <arsenium1941@gmail.com>
2021-03-13 21:05:51 +0500

    new_ssh

--- recipes_service/settings.py
+++ recipes_service/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = '(e4((3w)ri^6*+mmm%$q@z2u7xp%vs1jklag-*%kc6d7i2-dz('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '178.154.209.138']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '130.193.59.249']
 
 
 # Application definition
