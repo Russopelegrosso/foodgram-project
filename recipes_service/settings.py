70386a11655bcab92a65d7d0cff7acc22e7747dc
Arsen Diubua <arsenium1941@gmail.com>
2021-03-14 11:03:43 +0500

    settings

--- recipes_service/settings.py
+++ recipes_service/settings.py
@@ -137,5 +137,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-_STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 SITE_ID = 1
