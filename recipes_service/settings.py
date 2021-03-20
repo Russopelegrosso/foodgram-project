364dcf7049086d88cea618ba0a6ce6ccad4ba529
Arsen Dubua <Arsen Diubua>
2021-03-20 21:01:40 +0500

    fix

--- recipes_service/settings.py
+++ recipes_service/settings.py
@@ -83,12 +83,12 @@ if DEBUG:
 else:
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": 'postgres',
-            "USER": 'postgres',
-            "PASSWORD": 'postgres',
-            "HOST": 'db',
-            "PORT": 5432,
+            "ENGINE": os.environ.get("DB_ENGINE"),
+            "NAME": os.environ.get("DB_NAME"),
+            "USER": os.environ.get("DB_USER"),
+            "PASSWORD": os.environ.get("DB_PASSWORD"),
+            "HOST": os.environ.get("DB_HOST"),
+            "PORT": os.environ.get("DB_PORT"),
         }
     }
 
