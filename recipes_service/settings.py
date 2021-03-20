22c66a8927a924422d85c5a28235fb843fd010c7
Arsen Dubua <Arsen Diubua>
2021-03-20 20:15:07 +0500

    fix_docker

--- recipes_service/settings.py
+++ recipes_service/settings.py
@@ -84,11 +84,11 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.environ.get("DB_NAME"),
-            "USER": os.environ.get("DB_USER"),
-            "PASSWORD": os.environ.get("DB_PASSWORD"),
-            "HOST": os.environ.get("DB_HOST"),
-            "PORT": os.environ.get("DB_PORT"),
+            "NAME": 'postgres',
+            "USER": 'postgres',
+            "PASSWORD": 'postgres',
+            "HOST": 'db',
+            "PORT": 5432,
         }
     }
 
